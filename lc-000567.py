4d75d9b3ccc6f7d0acb11adc0a6d9b96a6ef773a
Grant Zeng <grant.zeng@student.unsw.edu.au>
2024-03-15 15:54:16 +1100

    working solution but still need to optimize to make frequency table equality check just work with a match variable

--- lc-000567.py
+++ lc-000567.py
@@ -1,11 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
+
         #
-        #   Basically, I get how to make the sliding window work
-        #   the issue is setting up a dictionary that I can edit each time
+        #   Working solution
+        #   - But stil am checking array equality rather than using a helper variable
         #
-        #   - Broken solution, need to fix
+        #   TODO: Implement having a match variable to check if states the same
+        #
+
+        # Populate frequency table of target
+        target = [0] * 26
+        for x in [ord(c) - ord('a') for c in s1]: target[x] += 1
+
+        # Sliding window: load frequency table then update when bigger than window
+        window = [0] * 26
+        B = [ord(c) - ord('a') for c in s2]
+
+        for i, x in enumerate(B):
+            # Update right bound
+            window[x] += 1
+
+            # Update left bound, if window has grown to full size and is shifting
+            if i >= len(s1): window[B[i - len(s1)]] -= 1
+
+            if window == target:
+                # Frequnecy table of current window is same as target, return
+                return True
+
+
+        return False
+
+        #
+        #   Broken solution trying to implement having a match variable to keep track
+        #   of whether two frequency tables explicitly the same
+        #
+
+        """
         if len(s1) > len(s2): return False
 
         window, whole = len(s1), len(s2)
@@ -20,7 +51,7 @@ class Solution:
 
         # Sliding window with updates to matches
         match = sum(1 if f1[i] == f2[i] else 0 for i in range(26))
-        for l, r in zip(range(whole - window), range(window, whole)):
+        for l, r in zip(range(whole - window + 1), range(window, whole)):
 
             # Previous window matched up completely
             if match == 26: return True
@@ -39,17 +70,12 @@ class Solution:
             elif f2[i2] + 1 == f1[i2]:  match -= 1
 
         return False
-
-
-
-
-
+        """
 
         #
-        #   This solution works but it's no longer O(n) because
-        #   you pay O(n) every time you rebuild frequency table
-        #   - Better to keep a table that you update as you go
-        #     or the trick of using 26 element long array for each letter
+        #   Working naive solution but is suboptimal because we're
+        #   rebuilding an entire frequency table each window step
+        #   rather than just updating it
         #
         """
         from collections import Counter 
