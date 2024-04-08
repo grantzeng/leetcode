ff7515945953cfc1d5f23218a988d31d232cbb4e
Grant Zeng <grant.zeng@student.unsw.edu.au>
2024-04-08 14:11:14 +1000

    78 bit manipulation solution

--- lc-000078.py
+++ lc-000078.py
@@ -1,6 +1,62 @@
 class Solution:
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        #
+        #
+        #   Slightly improved solution using for loops
+        #
+        #
+
+        n = len(nums)
+        subsets = []
+
+        for bitstring in range(2 ** n):
+            subset = []
+            for shift in range(n):
+                if bitstring & 1 << shift:
+                    subset.append(nums[shift])
+
+            subsets.append(subset)
+        return subsets
+        #
+        #
+        #   A solution using bit strings to pick out which elements!
+        #   - avoids use of stack/recursion so you don't get overflow
+        #
+        #   Comes from counting how many subsets there are in a set
+        #   Either chose ith or don't  so 2 ^ n possible choices
+        #
+        n = len(nums)
+        subsets = []
+        for bitstring in range(2 ** n):
+
+            subset = []
+            j = 0b1
+            idx = 0
+            while idx < n:
+                if j & bitstring:
+                    subset.append(nums[idx])
+                j = j << 1
+                idx += 1
+
+            subsets.append(subset)
+
+
+        return subsets
+
+        #
+        #
+        #   Iterative solution by DFSing with a stack
+        #
+        #
+
+        ## TODO:
+
+        #
+        #   Recursive solution using DFS
+        #
+        #
+
         n = len(nums)
         subsets = []
 
